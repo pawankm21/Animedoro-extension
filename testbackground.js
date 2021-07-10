ac4b5e1bf459a91568a41a0ebe1cb73ef9329429
pawan <b419039@iiit-bh.ac.in>
2021-07-11 03:28:43 +0530

    created basic timer

---
+++ testbackground.js
@@ -0,0 +1,88 @@
+var sessionDuration;
+var breakDuration;
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({
+    sessionDuration: 1,
+    breakDuration: 1,
+    endTime: null,
+    isTimerStarted: false,
+  });
+  console.log("initialized endTime=false,istimer=false");
+});
+chrome.storage.sync.get(["sessionDuration", "breakDuration"], (res) => {
+  sessionDuration = res.sessionDuration;
+  breakDuration = res.breakDuration;
+});
+chrome.alarms.onAlarm.addListener((alarm) => {
+  registration.showNotification("Arigato", {
+    body: alarm.name + " is Over!!",
+    icon: "images/46470_pandora_icon.png",
+  });
+  console.log(alarm.name + "Notification sent");
+  chrome.alarms.clear(alarm.name);
+  chrome.storage.sync.set({
+    endTime: null,
+    isTimerStarted: false,
+  });
+  console.log("endtime is set to nul and timerstarted set to false");
+});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.button == "session") {
+    registration.showNotification("Kon'nichiwa", {
+      body: "It's time for some work!!",
+      icon: "images/46470_pandora_icon.png",
+    });
+    console.log("bg got session message");
+    var time = new Date();
+    time.setMinutes(time.getMinutes() + sessionDuration);
+    console.log("bg endTime set to ", time, sessionDuration);
+    var endTime = time.toJSON();
+    chrome.storage.sync.set({
+      endTime: endTime,
+      isTimerStarted: true,
+    });
+    chrome.storage.sync.get(["sessionDuration"], (res) => {
+      console.log("endTime is set and isTimer is true");
+      chrome.alarms.create("Work", {
+        delayInMinutes: res.sessionDuration,
+      });
+    });
+    sendResponse("session started");
+  }
+  if (request.button == "break") {
+    registration.showNotification("Kon'nichiwa", {
+      body: "Come on, take a break!!",
+      icon: "images/46470_pandora_icon.png",
+    });
+    console.log("bg got break message");
+    var time = new Date();
+    time.setMinutes(time.getMinutes() + breakDuration);
+    console.log("bg endTime set to ", time, breakDuration);
+    var endTime = time.toJSON();
+    chrome.storage.sync.set({
+      endTime: endTime,
+      isTimerStarted: true,
+    });
+    chrome.storage.sync.get(["breakDuration"], (res) => {
+      console.log("endTime is set and isTimer is true");
+      chrome.alarms.create("Break", {
+        delayInMinutes: res.breakDuration,
+      });
+    });
+    sendResponse("break started");
+  }
+  if (request.button == "end") {
+    registration.showNotification("Ended", {
+      body: "timer is off",
+      icon: "images/46470_pandora_icon.png",
+    });
+    chrome.alarms.clearAll(() => {
+      chrome.storage.sync.set({
+        endTime: null,
+        isTimerStarted: false,
+      });
+      console.log("cleared all alarms");
+    });
+    sendResponse("ended");
+  }
+});
---
+++ testpopup.js
@@ -0,0 +1,81 @@
+var timer;
+var sessionDuration;
+var breakDuration;
+chrome.storage.sync.get(["sessionDuration,breakDuration"], (r) => {
+  sessionDuration = r.sessionDuration;
+  breakDuration = r.breakDuration;
+});
+$(function () {
+  ViewTimer.start();
+  $("#session").click(function (e) {
+    e.preventDefault();
+    var time = new Date();
+    time.setMinutes(time.getMinutes() + sessionDuration);
+    console.log("bg endTime set to ", time, sessionDuration);
+    var endTime = time.toJSON();
+    chrome.storage.sync.set({
+      endTime: endTime,
+    });
+    chrome.runtime.sendMessage({ button: "session" }, (r) => {
+      console.log("popup", r);
+      ViewTimer.start();
+    });
+  });
+  $("#break").click(function (e) {
+    e.preventDefault();
+    var time = new Date();
+    time.setMinutes(time.getMinutes() + sessionDuration);
+    console.log("bg endTime set to ", time, sessionDuration);
+    var endTime = time.toJSON();
+    chrome.storage.sync.set({
+      endTime: endTime,
+    });
+    console.log("break pushed");
+    chrome.runtime.sendMessage({ button: "break" }, (r) => {
+      console.log("popup", r);
+      ViewTimer.start();
+    });
+  });
+  $("#end").click(function (e) {
+    e.preventDefault();
+
+    chrome.runtime.sendMessage({ button: "end" }, (r) => {
+      chrome.alarms.clearAll();
+      console.log("popup", r);
+      ViewTimer.stop();
+    });
+  });
+});
+
+var ViewTimer = {
+  start: () => {
+    chrome.storage.sync.get(["isTimerStarted", "endTime"], (res) => {
+      if (res.isTimerStarted) {
+        $("#session").prop("disabled", true);
+        $("#break").prop("disabled", true);
+        var jsontime = res.endTime;
+        var endTime = new Date(jsontime);
+        console.log("popup", endTime);
+        timer = setInterval(() => {
+          var date = new Date();
+          var df = Math.floor((endTime - date) / 1000) + 1;
+          $("#time").html(`${Math.floor(df / 60)}:${df % 60}`);
+          if (!df) {
+            $("#time").html("00:00");
+            clearInterval(timer);
+            $("#session").prop("disabled", false);
+            $("#break").prop("disabled", false);
+          }
+        }, 1000);
+      } else {
+        $("#time").html("00:00");
+      }
+    });
+  },
+  stop: () => {
+    $("#time").html("00:00");
+    clearInterval(timer);
+    $("#session").prop("disabled", false);
+    $("#break").prop("disabled", false);
+  },
+};
